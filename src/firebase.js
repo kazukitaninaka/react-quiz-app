292cd815bdf0e8e8d20bfc7d1f7a068a2cc9e35a
Kazuki Taninaka <onelive555@gmail.com>
2021-12-08 22:55:53 +0900

    add env local

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBdlIgvMjpbJluy6z_3FbDq38wPzWM48tM",
-  authDomain: "react-quiz-app-a9149.firebaseapp.com",
-  databaseURL: "https://react-quiz-app-a9149-default-rtdb.firebaseio.com",
-  projectId: "react-quiz-app-a9149",
-  storageBucket: "react-quiz-app-a9149.appspot.com",
-  messagingSenderId: "176335193502",
-  appId: "1:176335193502:web:ce8926714008fa0d4a828f",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(config);
